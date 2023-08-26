9ea38279bec4de60920f553921fa06fe05edf150
Mehedihasan023 <mh20973@gmail.com>
2023-08-27 03:22:39 +0600

    send products by ids

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const app= express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fazp1r.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,8 +26,8 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const productCollection = client.db('emaJohnDB').collection('products');
-        
-        app.get('/products', async(req,res)=>{
+
+        app.get('/products', async (req, res) => {
             console.log(req.query)
             const page = parseInt(req.query.page) || 0;
             const limit = parseInt(req.query.limit) || 10;
@@ -35,11 +35,26 @@ async function run() {
             const result = await productCollection.find().skip(skip).limit(limit).toArray();
             res.send(result);
         })
-        
-        app.get('/totalProducts', async(req,res)=>{
+
+        app.get('/totalProducts', async (req, res) => {
             const result = await productCollection.estimatedDocumentCount();
-            res.send({totalProducts: result});
+            res.send({ totalProducts: result });
         })
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => new ObjectId(id))
+            const query = { _id: { $in: objectIds } };
+            const result = await productCollection.find(query).toArray();
+            res.send(result)
+            console.log(ids)
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -54,9 +69,9 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('john is busy shopping');
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`ema john server is running on port: ${port}`)
 })
